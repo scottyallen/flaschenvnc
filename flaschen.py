40d57d5bbfb2281996f2f4821bc61ec3ea2796ab
Scotty Allen <scotty@scottyallen.com>
2016-05-10 19:02:16 -0700

    Clean up python driver.

--- flaschen.py
+++ flaschen.py
@@ -1,29 +1,36 @@
 import socket
 
 class Flaschen(object):
-  def __init__(self, host, port, width, height):
+  def __init__(self, host, port, width, height, layer=0):
     self.host = host
     self.port = port
+    self.width = width
+    self.height = height
+    self.layer = layer
     self.pixels = []
     for x in xrange(width):
       self.pixels.append([(0, 0, 0) for y in xrange(height)])
     self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-  def send(self, data):
+  def _send(self, data):
     self.sock.sendto(data, (self.host, self.port))
 
-  def header(self):
+  def _header(self):
     return ''.join(["P6\n",
                     "45 35\n",
                     "255\n"])
 
-  def footer(self):
+  def _footer(self):
     return ''.join(["0\n",
                     "0\n",
-                    "14\n"])
+                    "%d\n" % self.layer])
 
-  def set(self, x, y, c):
-    self.pixels[x][y] = c
+  def set(self, x, y, color):
+    if x >= self.width or y >= self.height:
+      return
+    if color == (0, 0, 0):
+      color = (1, 1, 1)
+    self.pixels[x][y] = color
   
   def show(self):
     data = []
@@ -31,5 +38,4 @@ class Flaschen(object):
       for x in xrange(0, 45):
         data.append(''.join([chr(c) for c in self.pixels[x][y]]))
 
-    print self.header() + ''.join(data) + "\n" + self.footer()
-    self.send(self.header() + ''.join(data) + "\n" + self.footer())
+    self._send(self._header() + ''.join(data) + "\n" + self._footer())
